711e37f8f2665ec491eaf56ac728e033e8f0d5e0
h2m_kinoko <happy2mylife@gmail.com>
2019-02-14 15:33:48 +0900

    shot target.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function onLoad() {
   const marker = document.getElementById('id-cursor');
   marker.addEventListener('animationcomplete', evt => {});
 
-  const clickElement = document.getElementById('sample-obj');
+  const clickElement = document.getElementById('link-obj');
   clickElement.addEventListener(
     'click',
     evt => {
@@ -10,4 +10,22 @@ function onLoad() {
     },
     false
   );
+
+  const targetElements = [];
+  const targetIds = ['id-box-1', 'id-box-2'];
+  targetElements.push(document.getElementById('id-box-1'));
+  targetElements.push(document.getElementById('id-box-2'));
+
+  targetElements.forEach(e => {
+    e.addEventListener('click', evt => {
+      const index = targetIds.findIndex(id => evt.currentTarget);
+      if (index != -1) {
+        targetIds.splice(0, index);
+      }
+
+      if (targetIds.length == 0) {
+        location.href = 'http://mashandroom.org';
+      }
+    });
+  });
 }
